09348e3a84470057724e110138238ce14b2cf93f
Mauro Berlanda <mauro.berlanda@gmail.com>
2019-11-09 23:51:45 +0100

    Pointer Basics C++

---
+++ cslibrary/106/pointers.cc
@@ -0,0 +1,25 @@
+#include "stdio.h"
+
+int main()
+{
+  // Allocate the pointers x and y
+  int *x;
+  int *y;
+
+  x = new int;
+  y = new int;
+
+  *x = 42;
+  *y = 13;
+
+  printf("Pointer x: %p, y: %p\n", x, y);
+  printf("Pointee x: %d, y: %d\n", *x, *y);
+
+  y = x;
+  *y = 13;
+
+  printf("Pointer x: %p, y: %p\n", x, y);
+  printf("Pointee x: %d, y: %d\n", *x, *y);
+
+  return 0;
+}
