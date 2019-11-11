a53a99e596e11237ed14e1294ea60c91463e4659
Mauro Berlanda <mauro.berlanda@gmail.com>
2019-11-11 16:54:52 +0100

    103 Linked List Basics

---
+++ cslibrary/103/linked_list_basics.c
@@ -0,0 +1,78 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void ArrayTest(){
+  int scores[100];
+
+  scores[0] = 1;
+  scores[1] = 2;
+  scores[2] = 3;
+}
+
+struct node {
+  int data;
+  struct node* next;
+};
+
+struct node* BuildOneTwoThree()
+{
+  struct node* head = NULL;
+  struct node* second = NULL;
+  struct node* third = NULL;
+
+  head = malloc(sizeof(struct node));
+  second = malloc(sizeof(struct node));
+  third = malloc(sizeof(struct node));
+
+  head->data = 1;
+  head->next = second;
+
+  second->data = 2;
+  second->next = third;
+
+  third->data = 3;
+  third->next = NULL;
+
+  return head;
+};
+
+// Excercise
+struct node* BuildOneTwoThreeBis()
+{
+  struct node* head;
+  struct node* second;
+  struct node* third;
+
+  head = malloc(sizeof(struct node));
+  second = malloc(sizeof(struct node));
+  third = malloc(sizeof(struct node));
+
+  head->data = 1;
+  head->next = second;
+  second->data = 2;
+  second->next = third;
+  third->data = 3;
+
+  return head;
+};
+
+int Length(struct node* head)
+{
+  struct node *current = head;
+  int count = 0;
+
+  while (current != NULL) {
+    count++;
+    current = current->next;
+  }
+
+  return count;
+}
+
+int main()
+{
+  struct node *head = BuildOneTwoThreeBis();
+  printf("Length of head: %d\n", Length(head));
+  printf("Length of NULL: %d\n", Length(NULL));
+  return 0;
+}
