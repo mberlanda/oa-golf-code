8353d4a738a5fd537540fa2c6127f87f2819a0c2
Mauro Berlanda <mauro.berlanda@gmail.com>
2019-11-13 18:17:17 +0100

    105 Ex 2 GetNth

---
+++ cslibrary/105/02_getnth.c
@@ -0,0 +1,31 @@
+#include "node.h"
+
+// Given a list and an index, return the data
+// in the nth node of the list. The nodes are numbered from 0.
+// Assert fails if the index is invalid (outside 0..lengh-1).
+int GetNth(struct node* head, int index) {
+    struct node* current = head;
+    int count = 0;
+
+    if (index < 0) return -1;
+    if (current == NULL) return -1;
+    for (current = head; current != NULL; current = current->next)
+    {
+        if (count == index) {
+            return current->data;
+        }
+        count++;
+    }
+    // if index > than length
+    return -1;
+}
+
+int main()
+{
+    struct node* myList = BuildOneTwoThree();
+    if (GetNth(myList, 1) == 2)
+    {
+        return 0;
+    }
+    return 1;
+}
