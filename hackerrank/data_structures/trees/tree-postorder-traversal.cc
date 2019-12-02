3c5d62386e85b41e7daa3b8b0c70399bef32e1b1
Mauro Berlanda <mauro.berlanda@gmail.com>
2019-12-02 10:46:45 +0100

    hackerrank tree postorder traversal

---
+++ hackerrank/data_structures/trees/tree-postorder-traversal.cc
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+// https://www.hackerrank.com/challenges/tree-postorder-traversal/problem
+class Node
+{
+public:
+  int data;
+  Node *left;
+  Node *right;
+  Node(int d)
+  {
+    data = d;
+    left = NULL;
+    right = NULL;
+  }
+};
+
+class Solution
+{
+public:
+  Node *insert(Node *root, int data)
+  {
+    if (root == NULL)
+    {
+      return new Node(data);
+    }
+    else
+    {
+      Node *cur;
+      if (data <= root->data)
+      {
+        cur = insert(root->left, data);
+        root->left = cur;
+      }
+      else
+      {
+        cur = insert(root->right, data);
+        root->right = cur;
+      }
+
+      return root;
+    }
+  }
+
+  void postOrder(Node *root)
+  {
+    if (root == NULL) return;
+    postOrder(root->left);
+    postOrder(root->right);
+    std::cout << root->data << " ";
+  }
+}; //End of Solution
+
+int main()
+{
+
+  Solution myTree;
+  Node *root = NULL;
+
+  int t;
+  int data;
+
+  std::cin >> t;
+
+  while (t-- > 0)
+  {
+    std::cin >> data;
+    root = myTree.insert(root, data);
+  }
+
+  myTree.postOrder(root);
+  return 0;
+}
